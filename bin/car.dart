181e9f758c34f7699e786606354357a95744a174
mir666 <mirrabby633@gmail.com>
2024-09-27 23:25:11 +0600

    Initial commit

---
+++ bin/car.dart
@@ -0,0 +1,30 @@
+class Car{
+  String _brand;
+  String _model;
+  int _year;
+
+  Car(this._brand, this._model, this._year);
+
+  String showBrand(){
+    return _brand;
+  }
+
+  String showModel(){
+    return _model;
+  }
+
+  int showYear(){
+    return _year;
+  }
+
+  int carAge(int year){
+    int age = DateTime.now().year - _year;
+    return age;
+  }
+
+
+
+
+
+
+}
